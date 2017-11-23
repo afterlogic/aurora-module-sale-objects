948bb4b882bcb0b34b6d7c9a91e4d0b5c18932f5
sash04ek <a.ovcharov@gmail.com>
2017-11-23 09:51:16 +0300

    added Download and Product objects

---
+++ Classes/Product.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @copyright Copyright (c) 2017, Afterlogic Corp.
+ * @license AGPL-3.0 or AfterLogic Software License
+ *
+ * This code is licensed under AGPLv3 license or AfterLogic Software License
+ * if commercial version of the product was purchased.
+ * For full statements of the licenses see LICENSE-AFTERLOGIC and LICENSE-AGPL3 files.
+ */
+
+namespace Aurora\Modules\SaleObjects\Classes;
+
+/**
+ *
+ * @package SaleObjects
+ * @subpackage Classes
+ */
+
+class Product extends \Aurora\System\EAV\Entity
+{
+	protected $aStaticMap = array();
+
+	public function toResponseArray()
+	{
+		$aResponse = parent::toResponseArray();
+		$aResponse['ProductId'] = $this->EntityId;
+		return $aResponse;
+	}
+}
