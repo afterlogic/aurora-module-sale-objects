0948d265331c9b5809ce3aec5e0c6a5bb2a32ce5
sash04ek <a.ovcharov@gmail.com>
2017-11-29 13:42:23 +0300

    ProductGroup is added

---
+++ Classes/ProductGroup.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @copyright Copyright (c) 2017, Afterlogic Corp.
+ * @license AGPL-3.0 or AfterLogic Software License
+ *
+ * This code is licensed under AGPLv3 license or AfterLogic Software License
+ * if commercial version of the product was purchased.
+ * For full statements of the licenses see LICENSE-AFTERLOGIC and LICENSE-AGPL3 files.
+ */
+
+namespace Aurora\Modules\Sales\Classes;
+
+/**
+ *
+ * @package SaleObjects
+ * @subpackage Classes
+ */
+
+class ProductGroup extends \Aurora\System\EAV\Entity
+{
+	public function __construct($sModule)
+	{
+		$this->aStaticMap = array(
+			'Title'			=> array('string', ''),
+			'Description'	=> array('string', ''),
+			'Homepage'		=> array('string', ''),
+		);
+		parent::__construct($sModule);
+	}
+	
+	public function toResponseArray()
+	{
+		$aResponse = parent::toResponseArray();
+		$aResponse['Id'] = $this->EntityId;
+		return $aResponse;
+	}
+}
